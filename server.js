5c47b8e120dd9aef81d8b1f1a9dc98ae0518e485
Ben-vegabond <vega.ben18@gmail.com>
2023-07-22 00:32:31 -0500

    changed var names

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const html_routes = require('./routes/html-routes')
-const api_routes = require('./routes/api-routes')
-const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/html-routes')
+const apiRoutes = require('./routes/api-routes')
+const PORT = process.env.PORT || 3002;
 // dynamically set the port
 const app = express();
 
@@ -9,8 +9,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use(htmlRoutes)
+app.use(apiRoutes)
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
